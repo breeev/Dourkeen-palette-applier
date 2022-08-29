02fc33e7d29f1eddc002415624a6a3f113db86b2
BrevalF <breval.ferrari@gmail.com>
2022-08-29 23:39:27 +0200

    uuuhh...

---
+++ SETUP.py
@@ -0,0 +1,4 @@
+from os import system
+from tkinter.messagebox import showerror
+system('pip install Pillow adobe-color-swatch')
+showerror('ERROR','just trollin 🤣\neverything should be good now. Check your terminal window for potential errors.')
--- rgb16to8bit.py
+++ rgb16to8bit.py
@@ -1,13 +1,7 @@
-from os import system# execute commands
-from tkinter import Tk# Window
-from tkinter.filedialog import askopenfilename
-from PIL import Image# Image object, container
-from time import sleep# to wait
-system('swatch extract -i wind_chill_swatches.aco -o "coma-separated values because its actually where csv comes from"')
-with open('coma-separated values because its actually where csv comes from') as f:palette=f.read()
-palette=[tuple(int(j[i:i+2],16)for i in range(0,len(j),4))for j in[i.split(',')[2][1:]for i in palette.splitlines()[1:]]]
+with open('coma-separated values because its actually where csv comes from') as f:s=f.read()
+s=[tuple(int(j[i:i+2],16)for i in range(0,len(j),4))for j in[i.split(',')[2][1:]for i in s.splitlines()[1:]]]
 t=Tk()
-p=Image.open(askopenfilename(parent=t,title='Select a Picture (please)',filetypes=(
+p=Image.open(askopenfilename(parent=t,title='Open Picture',filetypes=(
    ('Portable Network Graphics','*.png'),
    ('Joint Photographic Experts Group',['*.jpg','*.jpeg']),
    ('Other files','*.*')
@@ -15,6 +9,7 @@ p=Image.open(askopenfilename(parent=t,title='Select a Picture (please)',filetype
 t.destroy()
 n=Image.new('RGB',p.size)
 nl=[]
+palette=[RGB(i)for i in palette]
 print('Working hard...')
 rgb=[None,None,None]
 for c in range(3):rgb[c]=[i[c] for i in palette]
